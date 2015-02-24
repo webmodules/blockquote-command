7065d65b90b3e23bf7392f3abc163a14480d031f
Nathan Rajlich <nathan@tootallnate.net>
2015-02-23 17:55:16 -0800

    test: add a couple failing test cases with BR nodes

--- test/test.js
+++ test/test.js
@@ -54,6 +54,37 @@ describe('BlockquoteCommand', function () {
         assert(range.endOffset === 1);
       });
 
+      it('should insert a BLOCKQUOTE element around parent block with a BR', function () {
+        div = document.createElement('div');
+        div.innerHTML = '<p><br></p>';
+        div.setAttribute('contenteditable', 'true');
+        document.body.appendChild(div);
+
+        // set current selection
+        var range = document.createRange();
+        range.setStart(div.firstChild, 0);
+        range.setEnd(div.firstChild, 0);
+        assert(range.collapsed);
+
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+
+        var blockquote = new BlockquoteCommand();
+        blockquote.execute();
+
+        // test that we have the expected HTML at this point
+        assert.equal('<blockquote><p><br></p></blockquote>', div.innerHTML);
+
+        // test that the Selection remains intact
+        sel = window.getSelection();
+        range = sel.getRangeAt(0);
+        assert(range.startContainer === div.firstChild.firstChild);
+        assert(range.startOffset === 0);
+        assert(range.endContainer === div.firstChild.firstChild);
+        assert(range.endOffset === 0);
+      });
+
       it('should remove a BLOCKQUOTE element when parent already contains one', function () {
         div = document.createElement('div');
         div.innerHTML = '<blockquote><p>hello</p></blockquote><p>world!</p>';
@@ -83,6 +114,37 @@ describe('BlockquoteCommand', function () {
         assert(range.startOffset === 1);
         assert(range.endContainer === div.firstChild.firstChild);
         assert(range.endOffset === 1);
+      });
+
+      it('should remove a BLOCKQUOTE element when parent with a BR already contains one', function () {
+        div = document.createElement('div');
+        div.innerHTML = '<blockquote><p><br></p></blockquote>';
+        div.setAttribute('contenteditable', 'true');
+        document.body.appendChild(div);
+
+        // set current selection
+        var range = document.createRange();
+        range.setStart(div.firstChild.firstChild, 0);
+        range.setEnd(div.firstChild.firstChild, 0);
+        assert(range.collapsed);
+
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+
+        var blockquote = new BlockquoteCommand();
+        blockquote.execute();
+
+        // test that we have the expected HTML at this point
+        assert.equal('<p><br></p>', div.innerHTML);
+
+        // test that the Selection remains intact
+        sel = window.getSelection();
+        range = sel.getRangeAt(0);
+        assert(range.startContainer === div.firstChild);
+        assert(range.startOffset === 0);
+        assert(range.endContainer === div.firstChild);
+        assert(range.endOffset === 0);
       });
 
       it('should remove a BLOCKQUOTE element when parent already contains one (2)', function () {
